ecc6c73c73cd5df75bc9b9f012ef4998423e5efa
muffintoxin <galina@nebenan.de>
2018-08-27 11:12:34 +0200

    added tests to fancyselect

--- test/fancy_select/index.js
+++ test/fancy_select/index.js
@@ -1,6 +1,6 @@
 const { createElement } = require('react');
 const { assert } = require('chai');
-const { mount } = require('enzyme');
+const { mount, shallow } = require('enzyme');
 
 const FancySelect = require('../../lib/fancy_select').default;
 
@@ -32,6 +32,8 @@ describe('<FancySelect />', () => {
 });
 
 describe('FancySelect', () => {
+  let wrapper;
+
   it('defaultValue', () => {
     const props = {
       defaultValue: 1,
@@ -40,9 +42,61 @@ describe('FancySelect', () => {
         { imageClass: 'c-sandbox-fancy_image_2', key: 'Zwei', value: 2 },
       ],
     };
-    const wrapper = mount(createElement(Child, props));
-    assert.equal(wrapper.instance().state.value, 555, 'default value is empty string');
+    wrapper = shallow(createElement(Child, props));
+    assert.equal(wrapper.instance().state.value, 555, 'default value is 555');
+  });
 
-    wrapper.unmount();
+  it('setValue', () => {
+    const props = {
+      defaultValue: 1,
+      options: [
+        { imageClass: 'c-sandbox-fancy_image_1', key: 'Eins', value: 1 },
+        { imageClass: 'c-sandbox-fancy_image_2', key: 'Zwei', value: 2 },
+      ],
+    };
+    wrapper = shallow(createElement(Child, props));
+
+    wrapper.instance().setValue('value');
+    assert.equal(wrapper.instance().state.value, 'value', 'set value');
+  });
+
+  it('setPristine', () => {
+    const props = {
+      defaultValue: 1,
+      options: [
+        { imageClass: 'c-sandbox-fancy_image_1', key: 'Eins', value: 1 },
+        { imageClass: 'c-sandbox-fancy_image_2', key: 'Zwei', value: 2 },
+      ],
+    };
+    wrapper = shallow(createElement(Child, props));
+    const instance = wrapper.instance();
+    instance.setValue('value');
+
+    assert.equal(instance.getValue(), 'value', 'value was updated');
+    assert.isFalse(instance.isPristine(), 'not pristine');
+
+    instance.setPristine();
+
+    assert.equal(instance.getValue(), 'value', 'value is same');
+    assert.isTrue(instance.isPristine(), 'pristine');
+  });
+
+  it('handleSelect', () => {
+    const props = {
+      defaultValue: 2,
+      options: [
+        { imageClass: 'c-sandbox-fancy_image_1', key: 'Eins', value: 1 },
+        { imageClass: 'c-sandbox-fancy_image_2', key: 'Zwei', value: 2 },
+      ],
+      deselectable: true,
+    };
+    wrapper = shallow(createElement(Child, props));
+
+    wrapper.instance().handleSelect(1);
+    assert.equal(wrapper.instance().state.value, undefined, 'deselectable value is undefined');
+
+    props.deselectable = false;
+    wrapper.instance().handleSelect(1);
+    assert.equal(wrapper.instance().state.value, 2, 'value is selectable and equal 2');
   });
 });

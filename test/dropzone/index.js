400085990bb0a9f1f9f3c25021c27cd6f8e5f2e0
muffintoxin <galina@nebenan.de>
2018-09-03 16:29:30 +0200

    added test for the dropzone component

--- test/dropzone/index.js
+++ test/dropzone/index.js
@@ -47,4 +47,140 @@ describe('<Dropzone />', () => {
 
     assert.equal(instance.deactivate.callCount, 1, 'deactivate was called');
   });
+
+  it('reset', () => {
+    const wrapper = mount(createElement(Dropzone));
+    const instance = wrapper.instance();
+
+
+    instance.reset();
+    assert.deepEqual(instance.state, instance.getDefaultState(), 'state is back to default');
+
+    wrapper.unmount();
+  });
+
+  it('activate', () => {
+    const wrapper = mount(createElement(Dropzone));
+    const instance = wrapper.instance();
+    instance.handleDragStart = fake();
+    instance.handleDragEnd = fake();
+    instance.handleGlobalDrop = fake();
+
+    const map = {};
+    document.addEventListener = fake((event, cb) => {
+      map[event] = cb;
+    });
+
+    instance.activate();
+    map.dragenter();
+    map.dragleave();
+    map.drop();
+
+    assert.equal(instance.handleDragStart.callCount, 1, 'handleDragStart was called');
+    assert.equal(instance.handleDragEnd.callCount, 1, 'handleDragEnd was called');
+    assert.equal(instance.handleGlobalDrop.callCount, 1, 'handleGlobalDrop was called');
+
+    wrapper.unmount();
+  });
+
+  it('deactivate', () => {
+    const wrapper = mount(createElement(Dropzone));
+    const instance = wrapper.instance();
+    instance.handleDragStart = fake();
+    instance.handleDragEnd = fake();
+    instance.handleGlobalDrop = fake();
+
+    const map = {};
+    document.removeEventListener = fake((event, cb) => {
+      map[event] = cb;
+    });
+
+    instance.deactivate();
+    map.dragenter();
+    map.dragleave();
+    map.drop();
+
+    assert.equal(instance.handleDragStart.callCount, 1, 'handleDragStart was called');
+    assert.equal(instance.handleDragEnd.callCount, 1, 'handleDragEnd was called');
+    assert.equal(instance.handleGlobalDrop.callCount, 1, 'handleGlobalDrop was called');
+
+    wrapper.unmount();
+  });
+
+  it('handleDragStart', () => {
+    const wrapper = mount(createElement(Dropzone));
+    const instance = wrapper.instance();
+
+    instance.handleDragStart();
+    assert.isTrue(instance.state.isActive, 'state was changed');
+
+    wrapper.unmount();
+  });
+
+  it('handleDragEnd', () => {
+    const wrapper = mount(createElement(Dropzone));
+    const instance = wrapper.instance();
+
+    instance.handleDragStart();
+    instance.handleDragEnd();
+    assert.isFalse(instance.state.isActive, 'state was changed');
+
+    wrapper.unmount();
+  });
+
+  it('handleGlobalDrop', () => {
+    const wrapper = mount(createElement(Dropzone));
+    const instance = wrapper.instance();
+
+    instance.handleDragStart();
+    instance.handleGlobalDrop();
+    assert.isFalse(instance.state.isActive, 'state was changed');
+
+    wrapper.unmount();
+  });
+
+  it('handleDragEnter', () => {
+    const wrapper = mount(createElement(Dropzone));
+    const instance = wrapper.instance();
+
+    instance.handleDragEnter();
+    assert.isTrue(instance.state.isHover, 'state was changed');
+
+    wrapper.unmount();
+  });
+
+  it('handleDragLeave', () => {
+    const wrapper = mount(createElement(Dropzone));
+    const instance = wrapper.instance();
+
+    instance.handleDragEnter();
+    instance.handleDragLeave();
+    assert.isFalse(instance.state.isHover, 'state was changed');
+
+    wrapper.unmount();
+  });
+
+  it('handleDragOver', () => {
+    const wrapper = mount(createElement(Dropzone));
+    const instance = wrapper.instance();
+    const preventDefault = fake();
+    const stopPropagation = fake();
+
+    instance.handleDragOver({ preventDefault, stopPropagation });
+    assert.equal(preventDefault.callCount, 1, 'preventDefault was called');
+    assert.equal(stopPropagation.callCount, 1, 'stopPropagation was called');
+
+    wrapper.unmount();
+  });
+
+  it('handleDrop', () => {
+    const wrapper = mount(createElement(Dropzone));
+    const instance = wrapper.instance();
+    const preventDefault = fake();
+
+    instance.handleDrop({ preventDefault, dataTransfer: { files: ['file1', 'file2'] } });
+    assert.equal(preventDefault.callCount, 1, 'preventDefault was called');
+
+    wrapper.unmount();
+  });
 });

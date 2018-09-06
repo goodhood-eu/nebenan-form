6ae0c3a2f99f1aa8773e79a05cb75bc970be8842
muffintoxin <galina@nebenan.de>
2018-09-06 16:32:55 +0200

    firxed for pr comments

--- test/dropzone/index.js
+++ test/dropzone/index.js
@@ -1,6 +1,6 @@
 const { createElement } = require('react');
 const { assert } = require('chai');
-const { mount, shallow } = require('enzyme');
+const { shallow } = require('enzyme');
 const { fake } = require('sinon');
 
 const Dropzone = require('../../lib/dropzone').default;
@@ -12,54 +12,63 @@ describe('<Dropzone />', () => {
       labelDrag: 'labelDrag',
       labelRelease: 'labelRelease',
     };
-    const wrapper = mount(createElement(Dropzone, props));
+    const wrapper = shallow(createElement(Dropzone, props));
+    const instance = wrapper.instance();
+    const labelDrag = wrapper.find('span .c-dropzone-overlay-text-active');
+    const labelRelease = wrapper.find('span .c-dropzone-overlay-text-hover');
+
     assert.lengthOf(wrapper.find('span .c-dropzone'), 1, 'dropzone was rendered');
-    assert.equal(wrapper.find('span .c-dropzone-overlay-text-active').length, 1, 'drag label was rendered');
-    assert.equal(wrapper.find('span .c-dropzone-overlay-text-hover').length, 1, 'release label was rendered');
+    assert.lengthOf(labelDrag, 1, 'drag label was rendered');
+    assert.lengthOf(labelRelease, 1, 'release label was rendered');
+
+    assert.equal(labelDrag.text(), 'labelDrag', 'drag label text was rendered');
+    assert.equal(labelRelease.text(), 'labelRelease', 'release label was rendered');
 
     // default state
-    assert.isFalse(wrapper.instance().state.isActive, 'default state for isActive is correct');
-    assert.isFalse(wrapper.instance().state.isHover, 'default state for isHover is correct');
-
-    wrapper.unmount();
+    assert.isFalse(instance.state.isActive, 'default state for isActive is correct');
+    assert.isFalse(instance.state.isHover, 'default state for isHover is correct');
   });
 
   // componentDidMount
-  it('should call activate', () => {
+  it('activate on mount', () => {
     const wrapper = shallow(createElement(Dropzone));
     const instance = wrapper.instance();
     instance.activate = fake();
 
     instance.componentDidMount();
-    assert.equal(instance.activate.callCount, 1, 'activate was called');
+    assert.isTrue(instance.activate.calledOnce, 'activate was called');
   });
 
   // componentWillUnmount
-  it('should call deactivate', () => {
+  it('deactivate on unmount', () => {
     const wrapper = shallow(createElement(Dropzone));
     const instance = wrapper.instance();
     instance.deactivate = fake();
 
     instance.componentWillUnmount();
-    assert.equal(instance.deactivate.callCount, 1, 'deactivate was called');
+    assert.isTrue(instance.deactivate.calledOnce, 'deactivate was called');
   });
 
   it('reset', () => {
-    const wrapper = mount(createElement(Dropzone));
+    const wrapper = shallow(createElement(Dropzone));
     const instance = wrapper.instance();
+
+    instance.handleDragStart();
+    instance.handleDragEnter();
+    assert.isTrue(instance.state.isActive, 'state was changed');
+    assert.isTrue(instance.state.isHover, 'state was changed');
 
     instance.reset();
     assert.deepEqual(instance.state, instance.getDefaultState(), 'state is back to default');
-
-    wrapper.unmount();
   });
 
   it('activate', () => {
-    const wrapper = mount(createElement(Dropzone));
+    const wrapper = shallow(createElement(Dropzone));
     const instance = wrapper.instance();
     instance.handleDragStart = fake();
     instance.handleDragEnd = fake();
     instance.handleGlobalDrop = fake();
+    const originalListener = document.addEventListener;
 
     const map = {};
     document.addEventListener = fake((event, cb) => {
@@ -75,15 +84,16 @@ describe('<Dropzone />', () => {
     assert.equal(instance.handleDragEnd.callCount, 1, 'handleDragEnd was called');
     assert.equal(instance.handleGlobalDrop.callCount, 1, 'handleGlobalDrop was called');
 
-    wrapper.unmount();
+    document.removeEventListener = originalListener;
   });
 
   it('deactivate', () => {
-    const wrapper = mount(createElement(Dropzone));
+    const wrapper = shallow(createElement(Dropzone));
     const instance = wrapper.instance();
     instance.handleDragStart = fake();
     instance.handleDragEnd = fake();
     instance.handleGlobalDrop = fake();
+    const originalListener = document.removeEventListener;
 
     const map = {};
     document.removeEventListener = fake((event, cb) => {
@@ -95,87 +105,120 @@ describe('<Dropzone />', () => {
     map.dragleave();
     map.drop();
 
-    assert.equal(instance.handleDragStart.callCount, 1, 'handleDragStart was called');
-    assert.equal(instance.handleDragEnd.callCount, 1, 'handleDragEnd was called');
-    assert.equal(instance.handleGlobalDrop.callCount, 1, 'handleGlobalDrop was called');
+    assert.isTrue(instance.handleDragStart.calledOnce, 'handleDragStart was called');
+    assert.isTrue(instance.handleDragEnd.calledOnce, 'handleDragEnd was called');
+    assert.isTrue(instance.handleGlobalDrop.calledOnce, 'handleGlobalDrop was called');
 
-    wrapper.unmount();
+    document.removeEventListener = originalListener;
   });
 
   it('handleDragStart', () => {
-    const wrapper = mount(createElement(Dropzone));
+    const props = { onDragStart: fake() };
+    const wrapper = shallow(createElement(Dropzone, props));
     const instance = wrapper.instance();
 
     instance.handleDragStart();
-    assert.isTrue(instance.state.isActive, 'state was changed');
+    instance.handleDragStart();
+    instance.handleDragStart();
 
-    wrapper.unmount();
+    assert.isTrue(instance.state.isActive, 'state was changed');
+    assert.isTrue(props.onDragStart.calledOnce, 'onDragStart was called only once');
   });
 
   it('handleDragEnd', () => {
-    const wrapper = mount(createElement(Dropzone));
+    const props = { onDragEnd: fake() };
+    const wrapper = shallow(createElement(Dropzone, props));
     const instance = wrapper.instance();
+
+    instance.handleDragStart();
+    instance.handleDragStart();
+    instance.handleDragEnd();
+    assert.isTrue(instance.state.isActive, 'state was not changed');
+    assert.isFalse(props.onDragEnd.calledOnce, 'onDragEnd was not called');
+
+    instance.handleGlobalDrop();
 
     instance.handleDragStart();
     instance.handleDragEnd();
     assert.isFalse(instance.state.isActive, 'state was changed');
-
-    wrapper.unmount();
+    assert.isTrue(props.onDragEnd.calledOnce, 'onDragEnd was called');
   });
 
   it('handleGlobalDrop', () => {
-    const wrapper = mount(createElement(Dropzone));
+    const wrapper = shallow(createElement(Dropzone));
     const instance = wrapper.instance();
+    instance.reset = fake();
 
-    instance.handleDragStart();
     instance.handleGlobalDrop();
-    assert.isFalse(instance.state.isActive, 'state was changed');
-
-    wrapper.unmount();
+    assert.isTrue(instance.reset.calledOnce, 'reset was called');
   });
 
   it('handleDragEnter', () => {
-    const wrapper = mount(createElement(Dropzone));
+    const props = { onDragEnter: fake() };
+    const wrapper = shallow(createElement(Dropzone, props));
     const instance = wrapper.instance();
 
     instance.handleDragEnter();
-    assert.isTrue(instance.state.isHover, 'state was changed');
+    instance.handleDragEnter();
+    instance.handleDragEnter();
 
-    wrapper.unmount();
+    assert.isTrue(instance.state.isHover, 'state was changed');
+    assert.isTrue(props.onDragEnter.calledOnce, 'onDragEnter was called only once');
   });
 
   it('handleDragLeave', () => {
-    const wrapper = mount(createElement(Dropzone));
+    const props = { onDragLeave: fake() };
+    const wrapper = shallow(createElement(Dropzone, props));
     const instance = wrapper.instance();
+
+    instance.handleDragEnter();
+    instance.handleDragEnter();
+    instance.handleDragEnter();
+    instance.handleDragLeave();
+    assert.isTrue(instance.state.isHover, 'state was not changed');
+    assert.isFalse(props.onDragLeave.calledOnce, 'onDragLeave was not called');
+
+    instance.handleGlobalDrop();
 
     instance.handleDragEnter();
     instance.handleDragLeave();
     assert.isFalse(instance.state.isHover, 'state was changed');
-
-    wrapper.unmount();
+    assert.isTrue(props.onDragLeave.calledOnce, 'onDragLeave was called');
   });
 
   it('handleDragOver', () => {
-    const wrapper = mount(createElement(Dropzone));
+    const props = { onDragOver: fake() };
+    const wrapper = shallow(createElement(Dropzone, props));
     const instance = wrapper.instance();
     const preventDefault = fake();
     const stopPropagation = fake();
 
     instance.handleDragOver({ preventDefault, stopPropagation });
-    assert.equal(preventDefault.callCount, 1, 'preventDefault was called');
-    assert.equal(stopPropagation.callCount, 1, 'stopPropagation was called');
-
-    wrapper.unmount();
+    assert.isTrue(preventDefault.calledOnce, 'preventDefault was called');
+    assert.isTrue(stopPropagation.calledOnce, 'stopPropagation was called');
+    assert.isTrue(props.onDragOver.calledOnce, 'onDragOver was called');
   });
 
   it('handleDrop', () => {
-    const wrapper = mount(createElement(Dropzone));
-    const instance = wrapper.instance();
+    const props = { onSelect: fake(), onDrop: fake() };
     const preventDefault = fake();
 
-    instance.handleDrop({ preventDefault, dataTransfer: { files: ['file1', 'file2'] } });
-    assert.equal(preventDefault.callCount, 1, 'preventDefault was called');
+    // without files
+    const wrapper = shallow(createElement(Dropzone, props));
+    const instance = wrapper.instance();
 
-    wrapper.unmount();
+    instance.handleDrop({ preventDefault, dataTransfer: { files: [] } });
+
+    assert.isTrue(preventDefault.calledOnce, 'preventDefault was called');
+    assert.isFalse(props.onSelect.calledOnce, 'onSelect was not called');
+    assert.isTrue(props.onDrop.calledOnce, 'onDrop was called');
+
+    // with files
+    const wrapperWithoutFiles = shallow(createElement(Dropzone, props));
+    const instanceWithoutFiles = wrapperWithoutFiles.instance();
+
+    instanceWithoutFiles.handleDrop({ preventDefault, dataTransfer: { files: ['file1', 'file2'] } });
+    assert.isTrue(props.onSelect.calledOnce, 'onSelect was called');
+    assert.isFalse(props.onDrop.calledOnce, 'onDrop was not called');
   });
 });
--- test/form/index.js
+++ test/form/index.js
@@ -1,6 +1,6 @@
 const { createElement } = require('react');
 const { assert } = require('chai');
-const { mount, shallow } = require('enzyme');
+const { shallow, mount } = require('enzyme');
 const { fake } = require('sinon');
 
 const Form = require('../../lib/form').default;
@@ -11,32 +11,49 @@ describe('<Form />', () => {
     const props = {
       alternativeAction: 'action',
       formError: 'Error',
+      buttonText: 'button',
+      buttonClass: 'buttonClass',
     };
-    const wrapper = mount(createElement(Form, props));
+    const wrapper = shallow(createElement(Form, props));
+    const error = wrapper.find('strong .c-form-error');
+    const button = wrapper.find('button .buttonClass');
 
     assert.lengthOf(wrapper.find('form .c-form'), 1, 'form was rendered');
     assert.lengthOf(wrapper.find('div .c-form-footer'), 1, 'footer was rendered');
-    assert.lengthOf(wrapper.find('strong .c-form-error'), 1, 'error was rendered');
 
-    wrapper.unmount();
+    assert.lengthOf(error, 1, 'error was rendered');
+    assert.equal(error.text(), 'Error', 'formError text was rendered correct');
+
+    assert.lengthOf(button, 1, 'button was rendered');
+    assert.equal(button.text(), 'button', 'button text  was rendered correct');
   });
 
   it('setPristine', () => {
     const wrapper = mount(createElement(Form));
     const instance = wrapper.instance();
+    const callback = fake();
+    const setPristine = fake();
+
     assert.isTrue(instance.state.isValid, 'default state is valild');
 
     instance.setValid(false);
     assert.isFalse(instance.state.isValid, 'state was changed');
 
-    instance.setPristine();
-    assert.deepEqual(instance.state, instance.getDefaultState(), 'state is back to default');
+    instance.addInput({ setPristine });
+    instance.setPristine(callback);
 
+    assert.deepEqual(instance.state, instance.getDefaultState(), 'state is back to default');
+    assert.isTrue(setPristine.calledOnce, 'setPristine on component was called');
+
+    setTimeout(() => {
+      assert.isTrue(callback.calledOnce, 'callback was called');
+    }, 100);
     wrapper.unmount();
   });
 
   it('setValid', () => {
-    const wrapper = mount(createElement(Form));
+    const props = { onValidityChange: fake() };
+    const wrapper = shallow(createElement(Form, props));
     const instance = wrapper.instance();
     const doneFake = fake();
 
@@ -44,40 +61,48 @@ describe('<Form />', () => {
 
     instance.setValid(false, doneFake);
 
-    assert.equal(doneFake.callCount, 1, 'callback was called');
+    assert.isTrue(doneFake.calledOnce, 'callback was called');
+    assert.isTrue(props.onValidityChange.calledOnce, 'onValidityChange prop was called');
     assert.isFalse(instance.state.isValid, 'state was changed');
-
-    wrapper.unmount();
   });
 
   it('isDisabled', () => {
     // this.props.locked is true
     const props = { locked: true };
-    const wrapperWithLock = mount(createElement(Form, props));
+    const wrapperWithLock = shallow(createElement(Form, props));
     const instanceWithLock = wrapperWithLock.instance();
     assert.isTrue(instanceWithLock.isDisabled(), 'form is disabled if locked is true');
-    wrapperWithLock.unmount();
 
     // this.state.isValid is false
-    const wrapper = mount(createElement(Form));
+    const wrapper = shallow(createElement(Form));
     const instance = wrapper.instance();
 
     instance.setValid(false);
     assert.isTrue(instance.isDisabled(), 'form is disabled if state is not valid');
-    wrapper.unmount();
+  });
+
+  it('updateValidity', () => {
+    const wrapper = shallow(createElement(Form));
+    const instance = wrapper.instance();
+    instance.setValid = fake();
+
+    instance.updateValidity();
+
+    assert.isTrue(instance.setValid.calledOnce, 'setValid was called');
   });
 
   it('reset', () => {
-    const wrapper = mount(createElement(Form));
+    const wrapper = shallow(createElement(Form));
     const instance = wrapper.instance();
-    assert.isTrue(instance.state.isValid, 'default state is valild');
+    const reset = fake();
 
+    instance.addInput({ reset });
     instance.setValid(false);
     assert.isFalse(instance.state.isValid, 'state was changed');
 
     instance.reset();
     assert.deepEqual(instance.state, instance.getDefaultState(), 'state is back to default');
-    wrapper.unmount();
+    assert.isTrue(reset.calledOnce, 'reset on component was called');
   });
 
   it('submit', () => {
@@ -87,8 +112,72 @@ describe('<Form />', () => {
     wrapper.find('form').simulate('submit', { preventDefault });
     const onSubmit = wrapper.props().onSubmit;
 
-    assert.equal(preventDefault.callCount, 1, 'preventDefault was called');
+    assert.isTrue(preventDefault.calledOnce, 'preventDefault was called');
     assert.exists(onSubmit, 'onSubmit prop exists');
     assert.typeOf(onSubmit, 'function', 'onSubmit must be a funciton');
+  });
+
+  it('validate', () => {
+    const wrapper = shallow(createElement(Form));
+    const instance = wrapper.instance();
+    instance.setValid = fake();
+
+    return Promise.all([
+      assert.isFulfilled(instance.validate(), 'success'),
+      assert.isRejected(instance.validate(), 'fail'),
+    ]);
+  });
+
+
+  it('addInput', () => {
+    const wrapper = shallow(createElement(Form));
+    const instance = wrapper.instance();
+
+    instance.addInput('input1');
+    instance.addInput('input2');
+    instance.addInput('input1');
+    instance.addInput('input2');
+    instance.addInput('input3');
+
+    assert.equal(instance.inputs.length, 3, 'only not existing input were added');
+  });
+
+  it('removeInput', () => {
+    const wrapper = shallow(createElement(Form));
+    const instance = wrapper.instance();
+
+    instance.addInput('input1');
+    instance.addInput('input2');
+    instance.addInput('input3');
+
+    instance.removeInput('input2');
+    instance.removeInput('input3');
+
+    assert.equal(instance.inputs.length, 1, 'input were removed');
+    assert.equal(instance.inputs[0], 'input1', 'correct input stayed');
+  });
+
+  it('isValid', () => {
+    const wrapper = shallow(createElement(Form));
+    const instance = wrapper.instance();
+    const isValid = fake();
+
+    instance.addInput({ isValid });
+
+    instance.isValid();
+
+    assert.isTrue(isValid.calledOnce, 'isValid was called');
+  });
+
+  it('isPristine', () => {
+    const wrapper = shallow(createElement(Form));
+    const instance = wrapper.instance();
+    const isPristine = fake();
+
+    instance.addInput({ isPristine });
+
+    instance.isPristine();
+
+    assert.isTrue(isPristine.calledOnce, 'isPristine was called');
   });
 });

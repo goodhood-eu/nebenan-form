6a89c6501ec72b4ee487ed46bef0290bdbf142b8
veeck <michael.veeck@nebenan.de>
2023-01-02 10:08:55 +0100

    Small cleanups

--- preview/containers/datepicker/index.jsx
+++ preview/containers/datepicker/index.jsx
@@ -12,51 +12,42 @@ export default () => {
 
   return (
     <article>
-      <Header noLink>nebenan-react-datepicker</Header>
+      <Header>React Datepicker</Header>
       <div className="preview-section">
         <ul>
           <li>
-
             <h3>Simple</h3>
             <DatePicker
               selected={selectedDate}
               onChange={setSelectedDate}
-
               {...defaults}
             />
           </li>
           <li>
-
             <h3>Preselected Date</h3>
             <DatePicker
               selected={preselectedDate}
               onChange={setPreselectedDate}
-
               {...defaults}
             />
           </li>
           <li>
-
             <h3>Min-Date</h3>
             <DatePicker
               selected={selectedDate}
               onChange={setSelectedDate}
               minDate={new Date(2020, 2, 1)}
-
               {...defaults}
             />
           </li>
           <li>
-
             <h3>Max-Date</h3>
             <DatePicker
               selected={selectedDate}
               onChange={setSelectedDate}
               maxDate={new Date(2020, 4, 1)}
-
               {...defaults}
             />
-
           </li>
           <li>
             <h3>Min-Date & Max-Date</h3>
@@ -65,7 +56,6 @@ export default () => {
               onChange={setSelectedDate}
               maxDate={new Date(2020, 4, 1)}
               minDate={new Date(2020, 2, 1)}
-
               {...defaults}
             />
           </li>
--- preview/containers/index/index.jsx
+++ preview/containers/index/index.jsx
@@ -8,7 +8,7 @@ export default () => (
       <ul className="ui-options">
         <li><Link to="/form">Form</Link></li>
         <li><Link to="/inputs">Inputs</Link></li>
-        <li><Link to="/datepicker">Datepicker</Link></li>
+        <li><Link to="/datepicker">React Datepicker</Link></li>
       </ul>
     </div>
   </article>

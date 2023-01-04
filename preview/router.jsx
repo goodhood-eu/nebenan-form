f24d12125d9b994b5321ba4cbd24a95423c0b1e9
veeck <michael.veeck@nebenan.de>
2023-01-04 10:48:25 +0100

    Remove react-datepicker preview

--- preview/router.jsx
+++ preview/router.jsx
@@ -4,7 +4,6 @@ import Error404 from './containers/error404';
 import Form from './containers/form';
 import Index from './containers/index';
 import Inputs from './containers/inputs';
-import DatePicker from './containers/datepicker';
 
 export default () => (
   <Switch>
@@ -12,7 +11,6 @@ export default () => (
 
     <Route path="/form" component={Form} />
     <Route path="/inputs" component={Inputs} />
-    <Route path="/datepicker" component={DatePicker} />
 
     <Route component={Error404} />
   </Switch>
